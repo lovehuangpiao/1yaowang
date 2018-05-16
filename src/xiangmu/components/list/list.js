31730207a4c12f81b4139957179547a4b23a144f
zhenzhong-yang <1564594495@qq.com>
2018-05-16 10:37:09 +0800

    重试

--- src/xiangmu/components/list/list.js
+++ src/xiangmu/components/list/list.js
@@ -4,9 +4,25 @@ import React from "react";
 
 
 export default class ListComponent extends React.Component{
-/*    componentWillMount(){
+    // componentWillMount(){
         
-    }*/
+    // }
+    state = {
+        data:[
+            {
+                id:123,
+                name:"三九/999 感冒灵颗粒 10g*9袋",
+                price:100,
+                img:"https://p4.maiyaole.com/img/item/201804/25/200_20180425163449933.jpg"
+            },{
+                id:125,
+                name:"三九/999 感冒灵颗粒 10g*9袋",
+                price:100,
+                img:"https://p4.maiyaole.com/img/item/201804/25/200_20180425163449933.jpg"
+            },
+
+        ]
+    }
     render(){
         return (
             <div id="list">
@@ -62,21 +78,26 @@ export default class ListComponent extends React.Component{
                     </li>
                 </ul>
                 <ul className="main">
-                    <li>
-                        <img src="https://p4.maiyaole.com/img/item/201804/25/200_20180425163449933.jpg"  className="list-img" />
-                        <div className="main-right-top">
-                            <p className="goods-name">
-                                <span className="ziying">自营</span>
-                                <span>三九/999 感冒灵颗粒 10g*9袋</span>
-                            </p>
-                            <p className="cheng">中成药</p><br/>
-                            <p className="taocan">套餐</p>
-                        </div>
-                        <div className="main-right-bottom">
-                            <span>￥10.2</span>
-                            <p>14161条评论 好评率99.30%<i className="fa fa-shopping-cart cart" aria-hidden="true"></i></p>
-                        </div>
-                    </li>
+                    {
+                        this.state.data.map(function(item){
+                            console.log(item);
+                            return <li key={item.id}>
+                                <img src={item.img}  className="list-img" />
+                                <div className="main-right-top">
+                                    <p className="goods-name">
+                                        <span className="ziying">自营</span>
+                                        <span>{item.name}</span>
+                                    </p>
+                                    <p className="cheng">中成药</p><br/>
+                                    <p className="taocan">套餐</p>
+                                </div>
+                                <div className="main-right-bottom">
+                                    <span>￥{item.price}</span>
+                                    <p>14161条评论 好评率99.30%<i className="fa fa-shopping-cart cart" aria-hidden="true"></i></p>
+                                </div>
+                            </li>
+                        })
+                    }
                 </ul>
             </div>
         )
