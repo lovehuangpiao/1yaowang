b9b4c9779b227f4f03a6a8b7aef52e2af6c7d258
zhenzhong-yang <1564594495@qq.com>
2018-05-16 12:29:36 +0800

    修改合并

--- src/xiangmu/component/common/common.js
+++ src/xiangmu/component/common/common.js
@@ -4,18 +4,15 @@ import React from "react"
 import {Link} from "react-router"
 
 export default class BodyComponent extends React.Component{
-
-    
     render(){
         return (
             <div className="yibody">
-                <Link to="/"><i className="fa fa-paper-plane-o" aria-hidden="true"></i><span>首页</span></Link>
-                <Link to="/zhaoyao"><i className="fa fa-drupal" aria-hidden="true"></i><span>找药</span></Link>
-                <Link to="/car"><i className="fa fa-shopping-cart" aria-hidden="true"></i><span>购物车</span></Link>
-                <Link to="/my"><i className="fa fa-user" aria-hidden="true"></i><span>我的</span></Link>
+                <Link to="/" className="home red"><i className="fa fa-paper-plane-o" aria-hidden="true"></i><span>首页</span></Link>
+                <Link to="/zhaoyao" className="zhaoyao"><i className="fa fa-drupal" aria-hidden="true"></i><span>找药</span></Link>
+                <Link to="/car" className="car"><i className="fa fa-shopping-cart" aria-hidden="true"></i><span>购物车</span></Link>
+                <Link to="/my" className="my"><i className="fa fa-user" aria-hidden="true"></i><span>我的</span></Link>
             </div>
         )
     }
+    
 }
-
-
